670b67d554dae914b2343b839cabc6bb2bc9711e
hirata-dat <49702207+hirata-dat@users.noreply.github.com>
2019-06-14 13:27:53 +0900

    復習0614

--- fukusyu0614/Form1.cs
+++ fukusyu0614/Form1.cs
@@ -12,9 +12,45 @@ namespace fukusyu0614
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//タイム
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            
+            if(label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top<0)
+            {
+                vy = -vy;
+            }
+            if(label1.Right>300)
+            {
+                vx = -vx;
+            }
+            if(label1.Bottom>300)
+            {
+                vy = -vy;
+            }
+            if (label1.Left > ClientSize.Width - label1.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)//ラベル
+        {
+
         }
     }
 }
